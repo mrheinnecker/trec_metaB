ca8de8c3e3718e53a55b08a8464b50cf866be139
Marco Rheinnecker <marco.rheinnecker@gmail.com>
2025-03-26 13:54:42 +0000

    improved documentation

--- map_metaB_tables.R
+++ map_metaB_tables.R
@@ -2,6 +2,7 @@ library(tidyverse)
 
 
 ## if not accessing via cluster node or jupyterhub, change paths to local schwab server mount location, e.g. Z:/schwab/
+## I recommend acces via jupyterhub R server: https://jupyterhub.embl.de
 mapping <- read_tsv("/g/schwab/Chandni/MetaB_AML_40um/trec-aml-ssuv4v5_dada2_counts.tsv")
 
 df_asvs <- read_tsv("/g/schwab/Chandni/MetaB_AML_40um/trec-aml-ssuv4v5_dada2_asvs.tsv/trec-aml-ssuv4v5_dada2_asvs.tsv")
