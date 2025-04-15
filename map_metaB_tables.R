f00a4d5bc7bf65493747664f773771d73a4e7c94
Marco Rheinnecker <marco.rheinnecker@gmail.com>
2025-04-15 09:22:00 +0000

    added plot for yannicks talk

--- map_metaB_tables.R
+++ map_metaB_tables.R
@@ -60,12 +60,14 @@ max_cols <- max(stringr::str_count(df_asvs$pr2_dada2_taxonomy, ";"), na.rm=T) + 
 
 taxo_anno <- df_asvs %>%
   select(asv_id, pr2_dada2_taxonomy) %>%
-  separate_wider_delim(pr2_dada2_taxonomy, delim = ";", names = paste0("level_", 1:max_cols), too_few = "align_start")
+  separate_wider_delim(pr2_dada2_taxonomy, delim = ";", 
+                       names = paste0("level_", 1:max_cols), 
+                       too_few = "align_start")
 
 
 
 
-plot_data <- site_mapping %>%
+plot_data_wide <- site_mapping %>%
   #filter(site==SOI) %>%
   left_join(mapping, by=c("err_id"="sample")) %>%
   group_by(site, asv_id) %>%
@@ -77,8 +79,10 @@ plot_data <- site_mapping %>%
   ) %>%
   left_join(
   taxo_anno
-    ) %>%
-  pivot_longer(cols = c("level_1", "level_2"),
+    ) 
+
+plot_data <- plot_data_wide %>%
+  pivot_longer(cols = c("level_2"),
                names_to = "level", values_to = "taxo")
   
 
@@ -102,11 +106,181 @@ dev.off()
 
 
 
+### yannicks artistic metaB figure
+
+tree_colors <- tibble(
+  taxo=factor(c("TSAR","Haptista","Cryptista","Archaeplastida","Amorphea","Obazoa","Excavata","CRuMs", "other")),
+  color=c("#b25545","#b08f89", "#8c8fc6", "#afb96c", "#959ca5","#959ca5", "#917990ff", "#ac9e71ff", "grey")
+)
+
+tree_pattern <- paste(tree_colors$taxo, collapse="|")
+
+site_order <- c("Villefranche","Reykjavik","Roscoff", "Tallinn","Kristineberg", "Bilbao","Porto")
+
+pd_mapped_to_tree <- plot_data_wide %>%
+  filter(level_1!="Bacteria") %>%
+  select(site, tot_reads, level_2) %>%
+  pivot_longer(cols = c("level_2"),
+               names_to = "level", values_to = "taxo") %>%
+  mutate(
+    
+    ## as the taxonomy annotations from ampliseq do not perfectly match with the one we have in the tree from:
+    ## https://www.cell.com/trends/ecology-evolution/fulltext/S0169-5347%2819%2930257-5#f0005
+    ## we have to map them manually:
+    taxo_new=case_when(
+      str_detect(taxo, tree_pattern) ~ str_extract(taxo, tree_pattern),
+      TRUE ~ "other"
+      #TRUE ~ taxo
+    ),
+    
+    ## to make it nicer
+    site_new=case_when(
+      str_detect(site, "Iceland") ~ "Reykjavik",
+      str_detect(site, "Villef") ~ "Villefranche",
+      TRUE ~ site
+    ) %>% factor(., levels=site_order),
+    
+    ## order correctly
+    
+    
+  )
+library(showtext)
+font_add_google("Gothic A1", "century")
+
+#p2_stack_v <- 
+  ggplot(
+    data=pd_mapped_to_tree,
+    aes(
+      x=site_new,
+      y=tot_reads, fill=factor(taxo_new, levels=tree_colors$taxo)
+    )
+  )+
+  #facet_wrap(~level, nrow=3)+
+  geom_col(position="stack", show.legend = F)+
+  scale_fill_manual(breaks = tree_colors$taxo, values = tree_colors$color)+
+  scale_  
+  #scale_x_discrete()+  
+  theme_bw(
+        base_family = "century",
+    )+
+  theme(
+    axis.text.x=element_text(angle=325, hjust=0),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+
+    #axis.title=element_blank(),
+    panel.grid = element_blank()
+  )
 
 
 
 
 
+pdf("/g/schwab/Marco/stack_v_smaller_fonts.pdf", width=9, height=6)
+p2_stack_v
+dev.off()
+
+
+
+
+png("/g/schwab/Marco/stack_v.png", width=600, height=400)
+p2_stack_v
+dev.off()
+
+
+
+
+
+### these not
+
+
+p2_fill_v <- 
+  ggplot(
+  data=pd_mapped_to_tree,
+  aes(
+    x=factor(site_new, levels=rev(sort(unique(pd_mapped_to_tree$site_new)))), y=tot_reads, fill=factor(taxo_new, levels=tree_colors$taxo)
+  )
+)+
+  #facet_wrap(~level, nrow=3)+
+  geom_col(position="fill", show.legend = F)+
+  scale_fill_manual(breaks = tree_colors$taxo, values = tree_colors$color)+
+  #scale_x_discrete()+  
+  theme_bw()+
+  theme(
+    axis.text.x=element_text(angle=325, hjust=0),
+    axis.text.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title=element_blank()
+  )
+
+
+
+p2_fill_h <- 
+  ggplot(
+    data=pd_mapped_to_tree,
+    aes(
+      x=site_new, y=tot_reads, fill=factor(taxo_new, levels=rev(tree_colors$taxo))
+    )
+  )+
+  #facet_wrap(~level, nrow=3)+
+  geom_col(position="fill", show.legend = F)+
+  scale_fill_manual(breaks = tree_colors$taxo, values = tree_colors$color)+
+  #scale_x_discrete()+  
+  theme_bw()+
+  theme(
+    #axis.text.y=element_text(angle=325, hjust=0),
+    axis.text.x = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title=element_blank()
+  )+
+  coord_flip()
+
+p2_stack_h <- 
+  ggplot(
+    data=pd_mapped_to_tree,
+    aes(
+      x=site_new, y=tot_reads, fill=factor(taxo_new, levels=rev(tree_colors$taxo))
+    )
+  )+
+  #facet_wrap(~level, nrow=3)+
+  geom_col(position="stack", show.legend = F)+
+  scale_fill_manual(breaks = tree_colors$taxo, values = tree_colors$color)+
+  #scale_x_discrete()+  
+  theme_bw()+
+  theme(
+    #axis.text.x=element_text(angle=325, hjust=0),
+    axis.text.x = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title=element_blank()
+  )+
+  coord_flip()
+
+
+
+
+# 
+# 
+# png("/g/schwab/Marco/fill_h.png", width=1600, height=1200, res=300)
+# p2_fill_h
+# dev.off()
+
+
+pdf("/g/schwab/Marco/fill_h.pdf", width=3, height=2)
+p2_fill_h
+dev.off()
+
+
+
+
+pdf("/g/schwab/Marco/fill_v.pdf", width=3, height=2)
+p2_fill_v
+dev.off()
+
+
+
+pdf("/g/schwab/Marco/stack_h.pdf", width=3, height=2)
+p2_stack_h
+dev.off()
 
 
 
